12322ac11763de000b41ae0b85c73bc8580dcdf2
Simon H Moore <simon@simonhugh.xyz>
2025-11-20 13:41:41 +0000

    refactor: delete old dotfiles to move to ansible

--- private_dot_config/private_mpv/private_scripts/private_yank-title.lua
+++
@@ -1,9 +0,0 @@
--- yank-title
--- 
--- yank media-title to clipboard
-
-mp.add_key_binding("y", "yank-title", function()
-    title = mp.get_property('media-title')
-    command = 'printf "%s" "' .. title .. '" | wl-copy &'
-    os.execute(command)
-end)
